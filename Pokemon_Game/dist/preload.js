f1335ad1b7e411860ca37345ac2b4d7ad2123e2c
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-08-01 22:31:03 +0200

    auto commit

--- Pokemon_Game/dist/preload.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-//# sourceMappingURL=preload.js.map
